495cd2940ff663e21fdf008658a5e936cee2b0bf
amyko <amyko@berkeley.edu>
2016-04-08 16:12:05 -0700

    pairwise test added

---
+++ pythonScripts/boxplot.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+import os.path
+
+testName = "test3"
+inPath = os.path.expanduser('~') + "/Google Drive/Research/pediBear/data/simulations/results/" + testName + ".kinship.acc"
+nIndiv = 6
+nPairs = nIndiv*(nIndiv-1)/2
+
+
+#dictionary to store accuracy rates
+accDict = dict()
+
+
+#read data
+infile = open(inPath)
+
+for line in infile:
+    
+    #pdb.set_trace()
+
+    fields = line.split()
+    if(fields[0]=='>'): continue
+
+    i = int(fields[0])
+    j = int(fields[1])
+    acc = float(fields[2])
+
+    if (i,j) in accDict:
+        accDict[(i,j)].append(acc)
+    else:
+        accDict[(i,j)] = [acc]
+
+
+infile.close()
+
+
+#make box plot
+data = [accDict[key] for key in accDict.keys()]
+means = [np.mean(x) for x in data]
+xdata = [i for i in range(1,nPairs+1)]
+
+#pdb.set_trace()
+
+plt.figure()
+plt.boxplot(data)
+#plt.scatter(xdata, means)
+plt.ylim([-.1,1.1])
+plt.xlabel("pair")
+plt.ylabel("accuracy")
+plt.title("pairwise accuracy for 100 simulations; " + testName)
+plt.show()
---
+++ src/test/RunPairwiseTest.java
@@ -0,0 +1,133 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+import statistic.Accuracy;
+import utility.DataParser;
+import dataStructures.Path;
+
+
+//infers the relationship from pairwise likelihood
+public class RunPairwiseTest {
+
+	
+	public static Path getHighestLkhdPath(String lkhdPath, int i, int j) throws NumberFormatException, IOException{
+
+		
+		//open reader
+		BufferedReader reader = DataParser.openReader(lkhdPath);
+		
+		Path bestPath = null;
+		double bestLkhd = Double.NEGATIVE_INFINITY;
+		Path currPath = null;
+		double currLkhd = Double.NEGATIVE_INFINITY;
+		
+		String line;
+		int currLineNum = 0;
+		while((line=reader.readLine())!=null){
+			
+			String[] fields = line.split("\t");
+			
+			//compare relationship
+			if(fields[0].equals(">")){
+				currPath = new Path(Integer.parseInt(fields[1]), Integer.parseInt(fields[2]), Integer.parseInt(fields[3]));
+				currLineNum = 0;
+				continue;
+			}
+			
+			
+			//find i and j
+			if(currLineNum==i){
+
+				currLkhd = Double.parseDouble(fields[j]);
+				
+				if(currLkhd > bestLkhd){
+					bestLkhd = currLkhd;
+					bestPath = currPath;
+				}
+				
+			}
+
+			currLineNum++;
+
+			
+		}
+
+		reader.close();
+		
+		return bestPath;
+					
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws IOException{
+		
+		//param
+		int numIndiv = 6;
+		
+		//files
+		String dir = System.getProperty("user.home") + "/Google Drive/Research/pediBear/data/simulations/";
+		String testName = "test3";
+		String inPath = dir + "pairwiseLikelihood/"+testName+".pairwise.";
+		String outPath = dir + "results/test.out";
+		String accPath = dir + "results/"+testName+".kinship.pairwise.acc";
+		String pathToOmegaPath = dir + "pathToOmega.txt";
+		String truePath = dir + "results/" +testName + ".true";
+		Map<Path, double[]> pathToKinship = Accuracy.getPathToOmega(pathToOmegaPath);
+		
+		//open outfiles
+		Writer writer = DataParser.openWriter(accPath);
+		
+		//run pairwise test
+		for(int t=0; t<100; t++){
+			
+			Writer writer2 = DataParser.openWriter(outPath);
+			writer2.write(String.format(">\t%d\n", t));
+			writer.write(String.format(">\t%d\n", t));
+			
+			String lkhdPath = inPath + t;
+			
+			
+			//pairwise result
+			for(int i=0; i<numIndiv; i++){
+				for(int j=i+1; j<numIndiv; j++){
+					
+					Path bestPath = getHighestLkhdPath(lkhdPath, i, j);
+					
+					writer2.write(String.format("%d\t%d\t%d\t%d\t%d\n", i, j, bestPath.getUp(), bestPath.getDown(), bestPath.getNumVisit()));
+					
+					
+				}
+			}
+			
+			writer2.close();
+			
+			//accuracy
+			double[][] kinshipAcc = Accuracy.kinshipAccuracy(outPath, truePath, numIndiv, pathToKinship);
+			for(int i=0; i<numIndiv; i++){
+				for(int j=i+1; j<numIndiv; j++){
+					writer.write(String.format("%d\t%d\t%.3f\n", i, j, kinshipAcc[i][j]));
+				}
+			}
+			
+			
+		}
+		
+		
+		
+		writer.close();
+
+		
+		
+		
+	}
+	
+	
+}
